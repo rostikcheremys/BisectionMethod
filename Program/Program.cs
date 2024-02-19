fbeb37f771e78002a32ca46cdd29ce5f216fe553
rostikсheremys <rostikcheremys@gmail.com>
2024-02-19 17:36:36 +0200

    fix errors

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Runtime.InteropServices;
 
 namespace Program
 {
@@ -30,21 +31,51 @@ namespace Program
         
         public static void Main()
         {
-            Console.Write("Введiть iнтервали через пробiл: ");
-            string[] input = Console.ReadLine().Split(' ');
-
-            double a = double.Parse(input[0]);
-            double b = double.Parse(input[1]);
+            double a = -6.283;
             
-            if (F(a) * F(b) > 0)
+            int count = 0;
+
+            double step = 0.4 * Math.PI;
+            
+            double[] xxx = new double[10]; 
+            double[] intervals = new double[10]; 
+            
+            for (int i = 0; count < 10; i++)
+            {
+                intervals[i] = F(a + i * step);
+                xxx[i] = (a + i * step);
+                count++;
+            }
+            
+            Console.WriteLine(string.Join(", ", xxx));
+
+           
+            int intervals1 = 0;
+            int intervals2 = 0;
+            
+            for (int i = 1; i < intervals.Length + 1; i++)
+            {
+                if (intervals[i] < 0)
+                {
+                    intervals2 = i;
+                    intervals1 = i - 1;
+                    break;
+                }
+            }
+            double toch1 = xxx[intervals1];
+            double toch2 = xxx[intervals2];
+            
+            
+            if (F(toch1) * F(toch2) > 0)
             {
                 Console.WriteLine("Неправильний iнтервал!");
                 return;
             }
             
-            double x = BisectionMethod(F, a, b, 0.1);
+            double x = BisectionMethod(F, toch1, toch2, 0.1);
+           
             
-            Console.WriteLine($"F(x): {F(x)}");
+            Console.WriteLine($"F(x): {F(Math.Round(x, 2))}");
         }
     }
 }
