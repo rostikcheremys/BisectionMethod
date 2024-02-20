b98dbe3307da6ed0e0be3ea60d4acf9185956402
rostikcheremys <rostikcheremys@gmail.com>
2024-02-20 11:47:34 +0200

    adding method

--- Program/Program.cs
+++ Program/Program.cs
@@ -2,20 +2,15 @@
 
 namespace Program
 {
-    abstract class Program
+    class Program
     {
-        private static double Function(double x)
-        {
-            return Math.Pow(x, 2) * Math.Cos(x) - 0.2;
-        }
-        
-        private static double BisectionMethod(Func<double, double> f, double a, double b, double eps)
+        private static double BisectionMethod(Func<double, double> function, double a, double b, double eps)
         {
             while (Math.Abs(a - b) > eps)
             {
                 double c = (a + b) / 2;
-                
-                if (f(a) * f(c) > 0)
+
+                if (function(a) * function(c) > 0)
                 {
                     a = c;
                 }
@@ -24,52 +19,58 @@ namespace Program
                     b = c;
                 }
             }
-            
+
             return (a + b) / 2;
         }
-        
-        public static void Main()
+
+        private static (double, double) Calculation(Func<double, double> function, double a, double b)
         {
-            double a = Convert.ToDouble(-2 * Math.PI);
-            double b = Convert.ToDouble(2 * Math.PI);
-            
             double step = (b - a) / 10;
-            
-            int count = 0;
+
             int intervalA = 0;
             int intervalB = 0;
 
-            double[] x = new double[10]; 
-            double[] fx = new double[10]; 
-            
-            for (int i = 0; count < 10; i++)
+            double[] x = new double[10];
+            double[] fx = new double[10];
+
+            for (int i = 0; i < 10; i++)
             {
-                fx[i] = Function(a + i * step);
                 x[i] = a + i * step;
-                count++;
+                fx[i] = function(a + i * step);
             }
-            
+
             for (int i = 1; i < fx.Length + 1; i++)
             {
-                if (fx[i] < 0)
+                if (fx[i - 1] * fx[i] < 0)
                 {
                     intervalB = i;
                     intervalA = i - 1;
                     break;
                 }
             }
-            
+
             double pointA = x[intervalA];
             double pointB = x[intervalB];
-            
-            if (Function(pointA) * Function(pointB) > 0)
+
+            if (function(pointA) * function(pointB) > 0)
             {
                 Console.WriteLine("Неправильний iнтервал!");
-                return;
             }
-            
-            double result = BisectionMethod(Function, pointA, pointB, 0.1);
-            Console.WriteLine($"F(x): {Function(result)}");
+
+            return (pointA, pointB);
+        }
+
+        public static void Main()
+        {
+            Func<double, double> function = x => Math.Pow(x, 2) * Math.Cos(x) - 0.2;
+
+            double a = -2 * Math.PI;
+            double b = 2 * Math.PI;
+
+            (double, double) interval = Calculation(function, a, b);
+
+            double result = BisectionMethod(function, interval.Item1, interval.Item2, 0.1);
+            Console.WriteLine($"F(x): {function(result)}");
         }
     }
 }
